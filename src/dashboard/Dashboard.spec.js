b5d1a7f9b2fa0e6d2f7b6cbb91018f3a368a643d
Suparada Saitalae <ann.suparada@gmail.com>
2019-08-19 16:17:42 -0500

    test render Controls and Displays

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import renderer from 'react-test-renderer'
+import { act } from 'react-dom/test-utils';
 
 import Dashboard from './Dashboard'
+import Controls from '../controls/Controls'
+import Display from '../display/Display'
 
 describe('<Dashboard />', () => {
     it('matches snapshot', () => {
         const tree = renderer.create(<Dashboard />)
         expect(tree.toJSON()).toMatchSnapshot();
     });
+
+    it('render Controls components ', () => {
+        const tree = renderer.create(<Controls />)
+        expect(tree.toJSON()).toMatchSnapshot();
+    })
+
+    it('render Display components ', () => {
+        const tree = renderer.create(<Display />)
+        expect(tree.toJSON()).toMatchSnapshot();
+    })
 });
