d27a9241f4ffbc36d19805befa4137f4df7e0d26
Suparada Saitalae <ann.suparada@gmail.com>
2019-08-19 18:42:31 -0500

    renders controls and displays

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import renderer from 'react-test-renderer'
-import { act } from 'react-dom/test-utils';
+import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 
-import Dashboard from './Dashboard'
-import Controls from '../controls/Controls'
-import Display from '../display/Display'
+import Dashboard from './Dashboard';
+
 
 describe('<Dashboard />', () => {
     it('matches snapshot', () => {
         const tree = renderer.create(<Dashboard />)
         expect(tree.toJSON()).toMatchSnapshot();
     });
-
-    it('render Controls components ', () => {
-        const tree = renderer.create(<Controls />)
-        expect(tree.toJSON()).toMatchSnapshot();
-    })
-
-    it('render Display components ', () => {
-        const tree = renderer.create(<Display />)
-        expect(tree.toJSON()).toMatchSnapshot();
-    })
+    it('renders controls and displays', () => {
+        const { getByText } = render(<Dashboard />);
+        getByText(/unlocked/i)
+        getByText(/open/i)
+        getByText(/lock gate/i)
+        getByText(/close gate/i)
+     }) 
 });
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer'
 
 import Display from './Display'
+
+
 
 describe('<Display />', () => {
     it('matches snapshot', () => {
         const tree = renderer.create(<Display />)
         expect(tree.toJSON()).toMatchSnapshot();
     });
+
+    
 });
 
