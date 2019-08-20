54682e1d7ba5a1386fb067bb622dffe8b91c2d93
Suparada Saitalae <ann.suparada@gmail.com>
2019-08-19 19:15:58 -0500

    Display test

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import renderer from 'react-test-renderer'
+import { render, cleanup } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import 'jest-dom/extend-expect';
 
 import Display from './Display'
 
@@ -11,7 +12,48 @@ describe('<Display />', () => {
         const tree = renderer.create(<Display />)
         expect(tree.toJSON()).toMatchSnapshot();
     });
-
-    
 });
 
+describe('test props Close ', () =>{
+    it("displays 'Closed' if the `closed` prop is `true`", () => {
+        const { getByText } = render(<Display closed={true} />)
+        getByText(/closed/i)
+    } )
+    it("displays 'Open' if the `closed` prop is `false`", () => {
+        const { getByText } = render(<Display open={false} />)
+        getByText(/open/i)
+    } )
+})
+
+describe('test Locked props', () => {
+    it("displays 'Locked' if the `locked` prop is `true`", () => {
+        const { getByText } = render(<Display locked={true} />)
+        getByText(/locked/i)
+    })
+    it("displays 'Unlocked' if the `locked` prop is `false`", () => {
+        const { getByText } = render(<Display unlocked={false} />)
+        getByText(/unlocked/i)
+    })
+})
+
+describe('red-led class test', () => {
+    it("when `locked` use the `red-led` class", () =>{
+        const { getByText } = render(<Display locked={true}/>)
+        expect(getByText(/locked/i)).toHaveClass('red-led')
+    })
+    it("when `Closed` use the `red-led` class", () =>{
+        const { getByText } = render(<Display closed={true}/>)
+        expect(getByText(/closed/i)).toHaveClass('red-led')
+    })
+})
+
+describe('green-led class test', () => {
+    it("when `unlocked` use the `green-led` class", () => {
+        const { getByText } = render(<Display unlocked={true} />)
+        expect(getByText(/unlocked/i)).toHaveClass('green-led')
+    })
+    it("when `open` use the `green-led` class", () => {
+        const { getByText } = render(<Display open={true} />)
+        expect(getByText(/open/i)).toHaveClass('green-led')
+    })
+})
